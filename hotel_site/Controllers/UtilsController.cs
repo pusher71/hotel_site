aa1f9c07e2f76c643d6f509c92cf1e63c5b1b463
pusher71 <cool.b987@yandex.ru>
2022-06-04 01:56:10 +0300

    Added UtilsController

---
+++ hotel_site/Controllers/UtilsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using hotel_site.Models;
+using hotel_site.Repository;
+
+namespace hotel_site.Controllers
+{
+    public class UtilsController : Controller
+    {
+        private readonly ILogger<UtilsController> _logger;
+        private readonly UserManager<User> _userManager;
+        private readonly IRepository<HotelInfo> _hotelInfoDb;
+        private readonly IRepository<HotelBuilding> _hotelBuildingDb;
+        private readonly IRepository<HotelPhoto> _hotelPhotoDb;
+        private readonly IRepository<Room> _roomDb;
+        private readonly IRepository<RoomPhoto> _roomPhotoDb;
+        private readonly IRepository<Book> _bookDb;
+        private readonly IRepository<Comment> _commentDb;
+        private readonly IRepository<Message> _messageDb;
+        private readonly IRepository<Service> _serviceDb;
+
+        public UtilsController(ILogger<UtilsController> logger,
+            UserManager<User> userManager,
+            HotelInfoDbRepository hotelInfoDb,
+            HotelBuildingDbRepository hotelBuildingDb,
+            HotelPhotoDbRepository hotelPhotoDb,
+            RoomDbRepository roomDb,
+            RoomPhotoDbRepository roomPhotoDb,
+            BookDbRepository bookDb,
+            CommentDbRepository commentDb,
+            MessageDbRepository messageDb,
+            ServiceDbRepository serviceDb)
+        {
+            _logger = logger;
+            _userManager = userManager;
+            _hotelInfoDb = hotelInfoDb;
+            _hotelBuildingDb = hotelBuildingDb;
+            _hotelPhotoDb = hotelPhotoDb;
+            _roomDb = roomDb;
+            _roomPhotoDb = roomPhotoDb;
+            _bookDb = bookDb;
+            _commentDb = commentDb;
+            _messageDb = messageDb;
+            _serviceDb = serviceDb;
+        }
+    }
+}
